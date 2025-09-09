e6e74a192f0a9992d3369e9e2e0a12003c84a6f2
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-08 21:54:38 -0300

    Delete Nível 1: JavaScript básico/exemplo01.js

--- Nível 1: JavaScript básico/exemplo01.js
+++
@@ -1,7 +0,0 @@
-//Olá, Mundo!
-
-console.log("Olá, Mundo!");
-
-//Para rodar esse código, salve-o em um arquivo e execute o comando: node exemplo01.js
-
-//Esse código imprime a mensagem "Olá, Mundo!" no console.
