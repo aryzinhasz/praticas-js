e91b21fe68929d202d96fc6272d8015a01ebc699
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-08 21:56:06 -0300

    arquivo.js

---
+++ Nível 1: JavaScript básico/exemplo01/arquivo.js
@@ -0,0 +1,10 @@
+//Interagindo com um botão
+
+const button = document.getElementById("meuBotao");
+button.addEventListener("click", function () {
+  alert("Botão clicado!");
+});
+
+//Para testar, crie um arquivo HTML com um botão que tenha o id "meuBotao" e vincule este script a ele.
+
+//Esse código adiciona um evento de clique a um botão no HTML, que exibe um alerta quando clicado.
