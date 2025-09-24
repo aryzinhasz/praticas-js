f618b967cd008cc012301d2de5b4b04953f68e8d
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:57:23 -0300

    Update arquivo.js

--- Nível 2: Condições em JavaScript/Exemplo18/arquivo.js
+++ Nível 2: Condições em JavaScript/Exemplo18/arquivo.js
@@ -1,1 +1,10 @@
+//Jogo da adivinhação
 
+let secreto = Math.floor(Math.random() * 10) + 1;
+let palpite = Number(prompt("Adivinhe o número entre 1 e 10:"));
+
+if (palpite === secreto) {
+  alert("Acertou! O número era " + secreto);
+} else {
+  alert("Errou! O número era " + secreto);
+}
