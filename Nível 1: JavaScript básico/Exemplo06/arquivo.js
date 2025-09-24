405f20df413b53bcf15b51973845b8d5d84e5f9e
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:46:11 -0300

    Update arquivo.js

--- Nível 1: JavaScript básico/Exemplo06/arquivo.js
+++ Nível 1: JavaScript básico/Exemplo06/arquivo.js
@@ -1,1 +1,7 @@
+//Somando dois números
 
+let a = Number(prompt("Digite o primeiro número:"));
+let b = Number(prompt("Digite o segundo número:"));
+alert("A soma é " + (a + b));
+
+//Esse código solicita ao usuário dois números, calcula a soma deles e exibe o resultado usando alert.
