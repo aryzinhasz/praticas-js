7fe46beec40b61173ebb69b5ad36a56a3920c173
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:55:55 -0300

    Update arquivo.js

--- Nível 2: Condições em JavaScript/Exemplo16/arquivo.js
+++ Nível 2: Condições em JavaScript/Exemplo16/arquivo.js
@@ -1,1 +1,6 @@
+//Calculando a idade
 
+let anoNasc = Number(prompt("Digite seu ano de nascimento:"));
+let anoAtual = new Date().getFullYear();
+let idade = anoAtual - anoNasc;
+alert("Você tem " + idade + " anos.");
