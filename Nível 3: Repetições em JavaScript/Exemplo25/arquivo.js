2ac74222506d51a6b44582b7bef5360b93881151
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 22:05:00 -0300

    Update arquivo.js

--- Nível 3: Repetições em JavaScript/Exemplo25/arquivo.js
+++ Nível 3: Repetições em JavaScript/Exemplo25/arquivo.js
@@ -1,1 +1,9 @@
+//Pegando dados de um formulário
 
+function mostrarDados(event) {
+  event.preventDefault();
+  let nome = document.getElementById("nome").value;
+  let idade = document.getElementById("idade").value;
+  document.getElementById("resultado").innerText =
+    "Olá " + nome + ", você tem " + idade + " anos.";
+}
