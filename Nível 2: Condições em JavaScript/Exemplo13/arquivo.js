9d3ee17d5541647f1f5e51f33d8e15630f8a86bf
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:53:53 -0300

    Update arquivo.js

--- Nível 2: Condições em JavaScript/Exemplo13/arquivo.js
+++ Nível 2: Condições em JavaScript/Exemplo13/arquivo.js
@@ -1,1 +1,12 @@
+//Qual é o maior?
 
+let a = Number(prompt("Digite o primeiro número:"));
+let b = Number(prompt("Digite o segundo número:"));
+
+if (a > b) {
+  alert("O maior é " + a);
+} else if (b > a) {
+  alert("O maior é " + b);
+} else {
+  alert("Os dois são iguais!");
+}
