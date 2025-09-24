19012d39dedc7047acf8912e7e727b4e81c85b5a
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 22:06:26 -0300

    Update arquivo.js

--- Nível 3: Repetições em JavaScript/Exemplo27/arquivo.js
+++ Nível 3: Repetições em JavaScript/Exemplo27/arquivo.js
@@ -1,1 +1,10 @@
+//Tabuada
 
+function tabuada() {
+  let n = Number(document.getElementById("num").value);
+  let res = "";
+  for (let i = 1; i <= 10; i++) {
+    res += `${n} x ${i} = ${n * i}\n`;
+  }
+  document.getElementById("resultado").innerText = res;
+}
