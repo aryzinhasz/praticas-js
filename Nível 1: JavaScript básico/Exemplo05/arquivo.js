5e41c586afc24f6dac71f7794d9704f0624ae109
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:45:24 -0300

    Update arquivo.js

--- Nível 1: JavaScript básico/Exemplo05/arquivo.js
+++ Nível 1: JavaScript básico/Exemplo05/arquivo.js
@@ -1,1 +1,8 @@
+//Manipulando Números com Prompt e Alert
 
+let n = Number(prompt("Digite um número:"));
+alert("O número digitado foi " + n);
+alert("O dobro é " + n * 2);
+alert("A metade é " + n / 2);
+
+//Esse código solicita ao usuário um número, calcula o dobro e a metade desse número, e exibe os resultados usando alert.
