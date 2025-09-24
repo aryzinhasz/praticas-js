27f73d09cdd54e8453ead8fe2e5e52a57990c17a
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:48:12 -0300

    Update arquivo.js

--- Nível 1: JavaScript básico/Exemplo08/arquivo.js
+++ Nível 1: JavaScript básico/Exemplo08/arquivo.js
@@ -1,1 +1,8 @@
+//Vários cálculos em JS
 
+let num = Number(prompt("Digite um número:"));
+alert("Número ao quadrado: " + num ** 2);
+alert("Raiz quadrada: " + Math.sqrt(num));
+alert("Dobro: " + num * 2);
+
+//Esse código solicita ao usuário um número, calcula o quadrado, a raiz quadrada e o dobro desse número, e exibe os resultados usando alert.
