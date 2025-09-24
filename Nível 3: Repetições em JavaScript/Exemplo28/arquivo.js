f0db2fa0180971603a19d0306b2da4e3e3b001f4
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 22:07:06 -0300

    Update arquivo.js

--- Nível 3: Repetições em JavaScript/Exemplo28/arquivo.js
+++ Nível 3: Repetições em JavaScript/Exemplo28/arquivo.js
@@ -1,1 +1,10 @@
+//Fatorial de um número
 
+function fatorial() {
+  let n = Number(document.getElementById("num").value);
+  let fat = 1;
+  for (let i = 1; i <= n; i++) {
+    fat *= i;
+  }
+  document.getElementById("resultado").innerText = `Fatorial de ${n} é ${fat}`;
+}
