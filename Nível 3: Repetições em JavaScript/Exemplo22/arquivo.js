61bee75a2950d38522ba90760b3c924acb18011c
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 22:02:40 -0300

    Update arquivo.js

--- Nível 3: Repetições em JavaScript/Exemplo22/arquivo.js
+++ Nível 3: Repetições em JavaScript/Exemplo22/arquivo.js
@@ -1,1 +1,11 @@
+//Contando de 1 até 10 (marcando os pares)
 
+let res = "";
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    res += "[" + i + "] ";
+  } else {
+    res += i + " ";
+  }
+}
+document.getElementById("resultado").innerText = res;
