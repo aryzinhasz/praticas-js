cb0ff16245585fac74b37413de35f3eec5348c98
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:52:22 -0300

    Update arquivo.js

--- Nível 2: Condições em JavaScript/Exemplo11/arquivo.js
+++ Nível 2: Condições em JavaScript/Exemplo11/arquivo.js
@@ -1,1 +1,11 @@
+//Calculando média e dando 'Parabéns'
 
+let n1 = Number(prompt("Digite a primeira nota:"));
+let n2 = Number(prompt("Digite a segunda nota:"));
+let media = (n1 + n2) / 2;
+
+if (media >= 7) {
+  alert("Parabéns! Sua média foi " + media);
+} else {
+  alert("Sua média foi " + media + ". Estude mais!");
+}
