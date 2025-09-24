ba4e7ef8fe872b5ff8a62db4221f494b4ca6c1b3
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:48:53 -0300

    Update arquivo.js

--- Nível 1: JavaScript básico/Exemplo09/arquivo.js
+++ Nível 1: JavaScript básico/Exemplo09/arquivo.js
@@ -1,1 +1,9 @@
+//Contador de cliques
 
+let contador = 0;
+function contar() {
+  contador++;
+  document.getElementById("resultado").innerText = "Cliques: " + contador;
+}
+
+//Esse código implementa um contador de cliques. Cada vez que o botão é clicado, a função contar() é chamada, incrementando o contador e atualizando o texto exibido na página HTML.
