8b7a30863d4cd032cde4249a14343e99f785dad7
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:58:18 -0300

    Update arquivo.js

--- Nível 2: Condições em JavaScript/Exemplo19/arquivo.js
+++ Nível 2: Condições em JavaScript/Exemplo19/arquivo.js
@@ -1,1 +1,25 @@
+//O usuário escolhe o cálculo
 
+let escolha = prompt(
+  "Escolha uma operação: soma, subtracao, multiplicacao, divisao"
+);
+
+let a = Number(prompt("Digite o primeiro número:"));
+let b = Number(prompt("Digite o segundo número:"));
+
+switch (escolha.toLowerCase()) {
+  case "soma":
+    alert("Resultado: " + (a + b));
+    break;
+  case "subtracao":
+    alert("Resultado: " + (a - b));
+    break;
+  case "multiplicacao":
+    alert("Resultado: " + a * b);
+    break;
+  case "divisao":
+    alert("Resultado: " + a / b);
+    break;
+  default:
+    alert("Operação inválida!");
+}
