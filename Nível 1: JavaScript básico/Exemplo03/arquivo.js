b7efd46e78d74a9279d3aedce91660876d129fdc
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:43:38 -0300

    Update arquivo.js

--- Nível 1: JavaScript básico/Exemplo03/arquivo.js
+++ Nível 1: JavaScript básico/Exemplo03/arquivo.js
@@ -1,1 +1,6 @@
+//Prompt e Alert
 
+let nome = prompt("Qual é o seu nome?");
+alert("Olá, " + nome + "! Seja bem-vindo.");
+
+//Esse código solicita o nome do usuário e exibe uma saudação personalizada usando alert.
