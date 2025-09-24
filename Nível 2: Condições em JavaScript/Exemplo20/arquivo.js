631a85d2348e9024c67a87f981b3a99678d18d60
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:58:59 -0300

    Update arquivo.js

--- Nível 2: Condições em JavaScript/Exemplo20/arquivo.js
+++ Nível 2: Condições em JavaScript/Exemplo20/arquivo.js
@@ -1,1 +1,18 @@
+//Estações do ano
 
+let mes = Number(prompt("Digite o número do mês (1 a 12):"));
+let estacao;
+
+if (mes === 12 || mes === 1 || mes === 2) {
+  estacao = "Verão";
+} else if (mes >= 3 && mes <= 5) {
+  estacao = "Outono";
+} else if (mes >= 6 && mes <= 8) {
+  estacao = "Inverno";
+} else if (mes >= 9 && mes <= 11) {
+  estacao = "Primavera";
+} else {
+  estacao = "Mês inválido";
+}
+
+alert("A estação é: " + estacao);
