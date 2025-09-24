5876d95fc52055e028d3a4a72a6ba7a0bb8132a8
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 21:44:27 -0300

    Update arquivo.js

--- Nível 1: JavaScript básico/Exemplo04/arquivo.js
+++ Nível 1: JavaScript básico/Exemplo04/arquivo.js
@@ -1,1 +1,7 @@
+//Interagindo com partes do HTML (DOM)
 
+function mudarTexto() {
+  document.getElementById("titulo").innerText = "O texto foi alterado!";
+}
+
+//Esse código altera o texto de um elemento HTML com o id "titulo" quando a função mudarTexto é chamada.
