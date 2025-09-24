1bef7eb6e780f5b420f028b9f415842d49ed8f18
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-23 22:06:03 -0300

    Update arquivo.js

--- Nível 3: Repetições em JavaScript/Exemplo26/arquivo.js
+++ Nível 3: Repetições em JavaScript/Exemplo26/arquivo.js
@@ -1,1 +1,23 @@
+//Contagem mais "inteligente"
 
+function contar(event) {
+  event.preventDefault();
+  let inicio = Number(document.getElementById("inicio").value);
+  let fim = Number(document.getElementById("fim").value);
+  let passo = Number(document.getElementById("passo").value);
+  let res = "";
+
+  if (passo <= 0) passo = 1;
+
+  if (inicio < fim) {
+    for (let i = inicio; i <= fim; i += passo) {
+      res += i + " ";
+    }
+  } else {
+    for (let i = inicio; i >= fim; i -= passo) {
+      res += i + " ";
+    }
+  }
+
+  document.getElementById("resultado").innerText = res;
+}
